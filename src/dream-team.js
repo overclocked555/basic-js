457b083f92554e64d305e08be42b2d61f1406288
Alexandr Khokhanov <hohanov@gmail.com>
2020-09-29 17:49:47 +0300

    feat: solve dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+    if (Array.isArray(members) === false) return false;
+    membersStrings = [];
+
+    members.forEach((element) => {
+      if (typeof element !== 'string') return;
+      element = element.trim();
+      if (element !== '') membersStrings.push(element.toUpperCase());
+      });
+
+    if (membersStrings.length === 0) return false;
+    membersStrings.sort();
+    commandName = '';
+    for (let name of membersStrings) {
+      commandName += name[0];
+    }
+
+    return commandName.toUpperCase();
+    }
