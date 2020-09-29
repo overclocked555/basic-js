27d104e07ff236bb326092062badc349f504c122
Alexandr Khokhanov <hohanov@gmail.com>
2020-09-29 12:47:29 +0300

    feat: resolve carbon-dating task

--- src/count-cats.js
+++ src/count-cats.js
@@ -3,13 +3,7 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function countCats(backyard) {
   sum = 0;
   backyard.forEach((element) => {
-
     sum += element.filter(function(char) {return char === '^^'}).length;
-    //console.log(sum);
-    
     });
   return (sum);
 };
-  //throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-//};
