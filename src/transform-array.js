e7917356adbd567c0f7dba69ec8278a60139cda1
Alexandr Khokhanov <hohanov@gmail.com>
2020-09-30 22:31:45 +0300

    feat: solve transform-array task (without advanced sequence interactions)

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,48 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+function verify(item) {
+    if (
+    item === '--discard-next' || 
+    item === '--discard-prev' ||
+    item === '--double-next' ||
+    item === '--double-prev' ||
+    item === undefined) 
+    return false; 
+    else return true;
+}
+
+module.exports = function transform(arr) {
+  if (Array.isArray(arr) === false) throw new Error();
+  newArr = [];
+  index = 0;
+    
+  for (index = 0; index < arr.length; ) {
+      if (arr[index-1] === '--discard-next') {
+          index++;
+          continue;}
+      
+      if (arr[index+1] === '--discard-prev') {
+          index++;
+          continue;
+      }
+
+      if (arr[index-1] === '--double-next') {
+          if (verify(arr[index])) newArr.push(arr[index]);
+          //if (verify(arr[index])) newArr.push(arr[index]);
+          //index++;
+          //continue;
+      }
+
+      if (arr[index+1] === '--double-prev') {
+          if (verify(arr[index])) newArr.push(arr[index]);
+          //if (verify(arr[index])) newArr.push(arr[index]);
+          //index++;
+          //continue;
+      }
+      
+      if (verify(arr[index])) newArr.push(arr[index]);
+
+      index++;
+  }
+  return newArr;
 };
