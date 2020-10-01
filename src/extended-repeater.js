b09cc21d6ee2049f66f2d36fd4ff195d432ec505
Alexandr Khokhanov <hohanov@gmail.com>
2020-10-01 11:07:19 +0300

    feat: solve advanced-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+function repeatWithSeparate(mainStr, repeatNum, separate) {
+  if (repeatNum === 0) return '';
+  return ((mainStr + separate).repeat(repeatNum - 1) + mainStr);
+}
+
+module.exports = function repeater(str, options) {
+  str = String(str);
+  if (options.separator === undefined) options.separator = '+';
+  if (options.addition === undefined) options.addition = '';
+  if (options.additionSeparator === undefined) options.additionSeparator = '|';
+  if (options.repeatTimes === undefined) options.repeatTimes = 1;
+  if (options.additionRepeatTimes === undefined) options.additionRepeatTimes = 1;
+  options.addition = String(options.addition);
+
+  newStr = repeatWithSeparate((str + (repeatWithSeparate(options.addition, options.additionRepeatTimes, options.additionSeparator))), options.repeatTimes , options.separator);
+  return newStr;
 };
   
