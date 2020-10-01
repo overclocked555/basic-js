a3157df076cf71e74cf9057257c3aad4448c5a43
Alexandr Khokhanov <hohanov@gmail.com>
2020-10-01 13:34:00 +0300

    feat: solve vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,51 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direction) {
+    if (direction === false) this.reverseDirection = true;
+    else this.reverseDirection = false;
+  }
+  
+  static verifyParams (message, key) {
+    if (message === undefined || key === undefined) throw new Error();
+  }
+
+  encrypt(message, key) {
+    VigenereCipheringMachine.verifyParams(message, key);
+    if (this.reverseDirection) message = message.split('').reverse().join('');
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let newMessage = '';
+    let keyCounter = 0;
+    for (var charCounter = 0; charCounter < message.length; charCounter++) {
+      if (/^[A-Z]$/.test(message[charCounter])) {
+        let code = (message.charCodeAt(charCounter) % 65 + key.charCodeAt(keyCounter) % 65) % 26 + 65;
+        newMessage += String.fromCharCode(code);
+        keyCounter = (keyCounter + 1) % key.length;
+      } else {
+        newMessage += message[charCounter];
+      }      
+    };
+    return newMessage;
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  decrypt(message, key) {
+    VigenereCipheringMachine.verifyParams(message, key);
+    if (this.reverseDirection) message = message.split('').reverse().join('');
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let newMessage = '';
+    let keyCounter = 0;
+    for (var charCounter = 0; charCounter < message.length; charCounter++) {
+      if (/^[A-Z]$/.test(message[charCounter])) {
+        let code = (26 + message.charCodeAt(charCounter) % 65 - key.charCodeAt(keyCounter) % 65) % 26 + 65;
+        newMessage += String.fromCharCode(code);
+        keyCounter = (keyCounter + 1) % key.length;
+      } else {
+        newMessage += message[charCounter];
+      }      
+    };
+    return newMessage;
   }
 }
 
